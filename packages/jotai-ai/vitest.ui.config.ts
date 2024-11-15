705bb49d848c32b4b229f0788c14d55eaf748cb2
Lanqing Huang <lqhuang@outlook.com>
2024-11-15 12:19:14 +0800

    chore: Add new dev deps and test suites

---
+++ packages/jotai-ai/vitest.ui.config.ts
@@ -0,0 +1,14 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: "jsdom",
+    globals: true,
+    includeSource: [
+      "src/**/*.ui.test.tsx",
+      // 'tests/**/*.?(c|m)[jt]s?(x)',
+    ],
+  },
+});
